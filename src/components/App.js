9a795c9e1ff91d8c02398933fbad219bd71c6d8e
viccon23 <victor.contreras23@aol.com>
2024-02-07 14:23:04 -0600

    Revamp #2! Still need a more aesthetic background, got multiple features added like visible scroll and project cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,92 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { useEffect } from 'react';
 import { Typewriter } from 'react-simple-typewriter'
 import '../styles/App.css';
 import me from '../images/me.jpg'; import wordie2Image from '../images/Wordie2.png';
+import github from '../images/github.png';
 
 
 
 
 function App() {
+  useEffect(() => {
+    // Add a scroll event listener
+    const handleScroll = () => {
+      const elements = document.querySelectorAll('.reveal-on-scroll');
+
+      elements.forEach((element) => {
+        if (isElementInViewport(element)) {
+          element.classList.add('visible');
+        }
+      });
+    };
+
+    // Attach the event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
   return (
     <>
       <NameTitle />
-      <div class="mainPage">
-        <div class="container">
-            <div class="content">
+      <div className="main-page">
+        <div className="container">
+          <div className="content">
             <Introduction />
-            </div>
-            <div class = "image">
-              <img src={me} alt="myProfileImage"/>
-            </div>
+          </div>
+          <div className="image">
+            <img src={me} alt="myProfileImage" />
+          </div>
         </div>
-      </div>  
+      </div> 
 
-      <div className="projects-list">
-          <Project
-            title=<h1>Python Wordle Clone</h1>
-            description="A Python project that clones the Wordle game."
-            imagePath={wordie2Image}
-            link="/Wordle"
-          />
-        </div>
+      <div className="container continuation reveal-on-scroll">
+        <p> Below are some Projects I have currently done:</p>
+      </div>
+
+      <div className="projectCards">
+          <div className="projects-list reveal-on-scroll">
+            <Project
+              title="Python Wordle Clone"
+              description="A Python project that clones the Wordle game."
+              imagePath={wordie2Image}
+              link="/Wordle"
+            />
+          </div>
+          <div className="projects-list reveal-on-scroll">
+            <Project
+              title=<h1>ChatGPT Discord Bot</h1>
+              description="Utilizing OpenAI's API, this custom made Discord bot will answer prompts from users who send messages through a text channel."
+              imagePath={wordie2Image}
+              link="/DiscordMessageBot"
+              githubUrl={"https://github.com/viccon23/GPTBot"}
+            />
+          </div>
+          <div className="projects-list reveal-on-scroll">
+            <Project
+              title=<h1>Python Wordle Clone</h1>
+              description="A Python project that clones the Wordle game."
+              imagePath={wordie2Image}
+              link="/Wordle"
+            />
+          </div>
+      </div>
     </>
+    
   );
 }
 
@@ -46,11 +102,7 @@ function Introduction() {
       </p>
 
       <p>
-    I am a graduate from Brownsville Early College High School, where I graduated with my Diploma and an Associate of Art in General Studies, effectively earning over 60 college credits before I began my college life. This allowed me to clear away my basics and start working with full focus and commitment on my core technology classes.
-    I am excited to see myself learn more as I continue my university studies, and can't wait to see what lies ahead. Thank you for visiting. 
-      </p>
-      <p>
-    Update as of November 4, 2023: Hello! I haven't taken the time to update this website or continue working on projects due to heavy schoolwork. However, I have been taking a Web Development course and will be revamping this website to use what I have learned, such as Server Side Templating, AJAX, and React instead of Flask. The new website will begin production in December once the semester finishes!
+      My current interests lie in Machine Learning, specifically classification, but I am still in the journey of discovering new software and concepts that could pique my interest somewhere else. I am also interested in Back-end development.  
       </p>
     </>
   )
@@ -77,17 +129,27 @@ function NameTitle() {
   )
 };
 
-function Project({ title, description, imagePath, link }) {
+function Project({ title, description, imagePath, tags, githubUrl }) {
   return (
     <div className="project-container">
-      <div className="project-info">
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <Link to={link}>Learn More</Link>
-      </div>
-      <div className="project-image">
-        <img src={imagePath} alt="WordleImage"/>
-      </div>
+        <div className="project-info">
+          <h2>{title}</h2>
+          <p>{description}</p>
+
+          <GitHubLink url={githubUrl} />
+        </div>
+        <div className="project-image">
+            <img src={imagePath} alt={title} />
+          </div>
     </div>
   );
 }
+
+function GitHubLink({ url }) {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <img src={github} alt="GitHub" style={{ width: '100px', height: '80px' }} />
+    </a>
+  );
+}
+
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import App from '../components/App';
-import Wordle from '../components/Wordle';
-import DiscordMessageBot from '../components/DiscordMessageBot';
-import CPPMusicPlaylist from '../components/CPPMusicPlaylist';
-import BankersAlgorithm from '../components/BankersAlgorithm';
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<App />} />
-        <Route path="/Wordle" element={<Wordle />} />
-        <Route path="/DiscordMessageBot" element={<DiscordMessageBot />} />
-        <Route path="/CPPMusicPlaylist" element={<CPPMusicPlaylist />} />
-        <Route path="/BankersAlgorithm" element={<BankersAlgorithm />} />
       </Routes>
     </Router>
   );
